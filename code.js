1b62390a55c89e8e2f819acb640495f359568048
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-15 11:34:57 -0400

    retest code.js

--- code.js
+++ code.js
@@ -1,21 +1,15 @@
 function fib(n) {
-
-    let array = [0,1];
-
+    let array = []
     if (n === 0) {
-
+        array = [0]
         return [0];
-
-    } else if(n === 1) {
-
-        return array;
-
-    } else {
-        for (let i = 2; i <= n; i++) {
-
-            array.push(array[i-1] + array[i-2]);
-            
-        }
+    } 
+   // if (n === 1) {
+     //   return [0,1];
+    //} 
+    array = [0,1]
+    for (i = 2; i <= n; i++) {
+        array.push(array[i-1] + array[i-2]);    
     }
     return array;
 }

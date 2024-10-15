d96c55c3b56e00586299c8ccae4dcc65dc699b28
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-15 09:52:59 -0400

    Modifications not finished yet

--- code.js
+++ code.js
@@ -8,10 +8,11 @@ function fib(n) {
 
     } 
     array = [0,1];
-    for (let i = 2; i <= n; i++) {
-        
+    for (let i=2; i<=n; i++){
+
         array.push(array[i-1] + array[i-2]);
-        
+
     }
+
     return array;
 }

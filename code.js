d64d4cb09f16d5a5700e4027ffaae7b9dfdbd198
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-15 09:48:01 -0400

    I think this is what I was supposed to do

--- code.js
+++ code.js
@@ -1,21 +1,17 @@
 function fib(n) {
 
-    let array = [0,1];
+    let array = [];
 
     if (n === 0) {
 
-        return [0];
-
-    } else if(n === 1) {
-
         return array;
 
-    } else {
-        for (let i = 2; i <= n; i++) {
-
-            array.push(array[i-1] + array[i-2]);
-            
-        }
+    } 
+    array = [0,1];
+    for (let i = 2; i <= n; i++) {
+        
+        array.push(array[i-1] + array[i-2]);
+        
     }
     return array;
 }

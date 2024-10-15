e666500df8973868d2d2ecabcfb2d7e56d528f76
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-15 09:57:23 -0400

    Unfinished

--- code.js
+++ code.js
@@ -1,15 +1,21 @@
 function fib(n) {
+
+    let array = [0,1];
+
     if (n === 0) {
-        
-        return 0;
-        
-    } else if (n === 1) {
-        
-        return 1; 
-        
+
+        return [0];
+
+    } else if(n === 1) {
+
+        return array;
+
     } else {
-        
-        return fib(n - 1) + fib(n - 2);
-        
+        for (let i = 2; i <= n; i++) {
+
+            array.push(array[i-1] + array[i-2]);
+            
+        }
     }
+    return array;
 }
